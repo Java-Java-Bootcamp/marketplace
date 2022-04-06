f03bcf92bf78a1777e8a9d97b7e609d24e74be24
Скворцов Демьян Алексеевич <6337520+slow3586@users.noreply.github.com>
2022-04-06 19:37:48 +0700

    WEB_CLIENT_URI теперь в properties, javadoc, limit параметр

--- telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegramBotRESTHandler.java
+++ telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegramBotRESTHandler.java
@@ -1,30 +1,32 @@
 package ru.teamtwo.telegrambot;
 
-import org.glassfish.jersey.internal.util.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import ru.teamtwo.telegrambot.dtos.ProductDTO;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 public class TelegramBotRESTHandler {
 
-    private static Logger logger = LoggerFactory.getLogger(TelegramBotRESTHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(TelegramBotRESTHandler.class);
 
-    private static final String WEB_CLIENT_URI = "http://localhost:8080";
+    @Value("${telegrambot.rest.webClientUri}")
+    private static String WEB_CLIENT_URI;
     private static final String PRODUCT_OFFERS_URI = "product-offers";
     private static final String FILTER_PARAMETER = "filter";
     private static final String OFFSET_PARAMETER = "offset";
+    private static final String LIMIT_PARAMETER = "limit";
     private static final String ORDER_PARAMETER = "order";
-
     private final WebClient webClient = WebClient.create(WEB_CLIENT_URI);
 
+    /**
+     * Виды сортировки по полям товара для запросов товаров
+     */
     public enum OrderType{
         PRICE("price"),
         PRODUCT_RATING("rating"),
@@ -35,6 +37,9 @@ public class TelegramBotRESTHandler {
         }
     }
 
+    /**
+     * Виды сортировки - по убывающей/возрастающей
+     */
     public enum OrderTypeAscDesc{
         ASC("asc"),
         DESC("desc");
@@ -44,14 +49,24 @@ public class TelegramBotRESTHandler {
         }
     }
 
-    public List<ProductDTO> getSortedProductsByFilterWithOffset(String filter, OrderType orderType, OrderTypeAscDesc ascDesc, int offset){
-        logger.debug("getSortedProductsByFilterWithOffset({},{},{},{})",filter,orderType,ascDesc,offset);
+    /**
+     * Запрашивает список товаров через REST API с указанными параметрами.
+     * @param filter Текстовый фильтр
+     * @param orderType Поле по которому будет сортировка
+     * @param ascDesc Сортировка по возрастающей/убывающей
+     * @param offset Сдвиг от первого товара в результатах поиска
+     * @param limit Максимальное кол-во товаров в листе
+     * @return Отсортированный, отфильтрованный список товаров
+     */
+    public List<ProductDTO> getSortedProductsByFilterWithOffsetAndLimit(String filter, OrderType orderType, OrderTypeAscDesc ascDesc, int offset, int limit){
+        logger.debug("getSortedProductsByFilterWithOffset({},{},{},{},{})",filter,orderType,ascDesc,offset,limit);
 
         StringBuilder uri = new StringBuilder();
         uri.append(PRODUCT_OFFERS_URI);
         uri.append("?");
         uri.append(FILTER_PARAMETER).append("=").append(filter).append("&");
         uri.append(OFFSET_PARAMETER).append("=").append(offset).append("&");
+        uri.append(LIMIT_PARAMETER).append("=").append(limit).append("&");
         uri.append(ORDER_PARAMETER).append("=").append(ascDesc).append("_").append(orderType.text);
         logger.debug("getSortedProductsByFilterWithOffset URI:{}", uri);
 
