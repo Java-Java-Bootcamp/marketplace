6a6d3418d802e44d8fb54826b215089d1b418014
Ykr8891 <ykr8891@gmail.com>
2022-04-06 14:50:04 +0300

    return spring app start code

--- telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegrambotApplication.java
+++ telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegrambotApplication.java
@@ -2,20 +2,11 @@ package ru.teamtwo.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class TelegrambotApplication {
 
     public static void main(String[] args) {
-        try {
-            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            botsApi.registerBot(new TelegramBot());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+        SpringApplication.run(TelegrambotApplication.class, args);
         }
     }
-
-}
