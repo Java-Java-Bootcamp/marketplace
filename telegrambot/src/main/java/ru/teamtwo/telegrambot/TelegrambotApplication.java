0c6e0a4854993aabdca92d1bbb32c56bfc524a82
Ykr8891 <ykr8891@gmail.com>
2022-04-06 14:36:12 +0300

    add bot instantiation to app add updatehandler to TelegramBot

--- telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegrambotApplication.java
+++ telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegrambotApplication.java
@@ -2,12 +2,24 @@ package ru.teamtwo.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class TelegrambotApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TelegrambotApplication.class, args);
+//        SpringApplication.run(TelegrambotApplication.class, args);
+        try {
+            // Create the TelegramBotsApi object to register your bots
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+
+            // Register your newly created AbilityBot
+            botsApi.registerBot(new TelegramBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
 }
