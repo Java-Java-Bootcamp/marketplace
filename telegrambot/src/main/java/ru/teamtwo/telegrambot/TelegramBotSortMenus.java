a4c295292ef3128f7a3cff06fa9e65a0f5cf3fd9
Скворцов Демьян Алексеевич <6337520+slow3586@users.noreply.github.com>
2022-04-08 14:28:13 +0700

    bean вместо простого singleton-а

--- telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegramBotSortMenus.java
+++ telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegramBotSortMenus.java
@@ -1,24 +1,23 @@
 package ru.teamtwo.telegrambot;
 
+import org.springframework.context.annotation.Bean;
+
 /**
  * Хранит в себе менюшки для сортировки товаров
  */
 public class TelegramBotSortMenus {
 
-    private static TelegramBotMenu sortFieldKeyboard;
-    private static TelegramBotMenu sortAscDescKeyboard;
-
     /**
      * Возвращает меню для сортировки по полю товара: цена, рейтинг и т.д.
      * @return Меню
      */
+    @Bean
     public static TelegramBotMenu getSortByFieldKeyboard(){
-        if(sortFieldKeyboard==null){
-            sortFieldKeyboard = new TelegramBotMenu();
-            sortFieldKeyboard.addRow("Цена");
-            sortFieldKeyboard.addRow("Рейтинг товара");
-            sortFieldKeyboard.addRow("Рейтинг продавца");
-        }
+        TelegramBotMenu sortFieldKeyboard = new TelegramBotMenu();
+        sortFieldKeyboard.addRow("Цена");
+        sortFieldKeyboard.addRow("Рейтинг товара");
+        sortFieldKeyboard.addRow("Рейтинг продавца");
+
         return sortFieldKeyboard;
     }
 
@@ -26,12 +25,12 @@ public class TelegramBotSortMenus {
      * Возвращает меню для сортировки по убыванию/возрастанию
      * @return Меню
      */
+    @Bean
     public static TelegramBotMenu getSortByAscDescKeyboard(){
-        if(sortAscDescKeyboard ==null) {
-            sortAscDescKeyboard = new TelegramBotMenu();
-            sortAscDescKeyboard.addRow("По возрастанию");
-            sortAscDescKeyboard.addRow("По убыванию");
-        }
+        TelegramBotMenu sortAscDescKeyboard = new TelegramBotMenu();
+        sortAscDescKeyboard.addRow("По возрастанию");
+        sortAscDescKeyboard.addRow("По убыванию");
+
         return sortAscDescKeyboard;
     }
 }
