018d896e2a4ebb79bda87e05d5c0e77b9f9f92da
Скворцов Демьян Алексеевич <6337520+slow3586@users.noreply.github.com>
2022-04-11 18:50:36 +0700

    больше менюшек

--- telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegramBotInlineMenus.java
+++ telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegramBotInlineMenus.java
@@ -5,32 +5,64 @@ package ru.teamtwo.telegrambot;
  */
 public class TelegramBotInlineMenus {
 
+    public static final String MENU_ORDER_DETAILS_TEXT = "Заказать";
+    public static final String MENU_ORDER_ADD_TEXT = "Добавить в корзину";
+    public static final String MENU_ORDER_QUANTITY_OK_TEXT = "Подтвердить";
+    public static final String MENU_ORDER_FINISH_TEXT = "Закончить выбор";
+    public static final String MENU_ORDER_ADDRESS_TEXT = "Подтвердить адрес";
+
     public static final String CALLBACK_ORDER_DETAILS_HEADER = "CallbackOrderDetails";
-    public static final String CALLBACK_ORDER_ACCEPT_HEADER = "CallbackOrderAccept";
+    public static final String CALLBACK_ORDER_ADD_HEADER = "CallbackOrderAdd";
+    public static final String CALLBACK_ORDER_QUANTITY_OK_HEADER = "CallbackOrderQuantity";
+    public static final String CALLBACK_ORDER_FINISH_HEADER = "CallbackOrderFinish";
+    public static final String CALLBACK_ORDER_ADDRESS_HEADER = "CallbackOrderAddress";
+
     public static final String CALLBACK_ORDER_ID_PARAM = "Id";
 
-    /**
-     * Менюшка с кнопкой "заказать", отображающаяся под постом с кратким описанием товара
-     * @param productId ID товара
-     * @return Меню для setReplyMarkup
-     */
-    public TelegramBotInlineMenu createOrderButton(String productId){
-        return TelegramBotInlineMenu.createMenuWithOneButton("Заказать",
-                CALLBACK_ORDER_DETAILS_HEADER+
+    public TelegramBotInlineMenu createButton(String text, String callback, String productId){
+        return TelegramBotInlineMenu.createMenuWithOneButton(text,
+                callback+
                         "," +
                         CALLBACK_ORDER_ID_PARAM+"="+productId);
     }
 
     /**
-     * Менюшка с кнопкой "принять", отображающаяся под постом
-     * с детальным описанием товара, под которым была нажата кнопка "заказать"
-     * @param productId ID товара
-     * @return Меню для setReplyMarkup
+     * Менюшка под постом с кратким описанием товара
      */
-    public TelegramBotInlineMenu createAcceptButton(String productId){
-        return TelegramBotInlineMenu.createMenuWithOneButton("Подтвердить",
-                CALLBACK_ORDER_ACCEPT_HEADER+
-                        ","+
-                        CALLBACK_ORDER_ID_PARAM+"="+productId);
+    public TelegramBotInlineMenu createDetailsButton(String productId){
+        return createButton(MENU_ORDER_DETAILS_TEXT, CALLBACK_ORDER_DETAILS_HEADER, productId);
+    }
+
+    /**
+     * Менюшка под постом
+     * с детальным описанием товара, под которым была нажата кнопка "заказать"
+     */
+    public TelegramBotInlineMenu createAddButton(String productId){
+        return createButton(MENU_ORDER_ADD_TEXT, CALLBACK_ORDER_FINISH_HEADER, productId);
+    }
+
+    /**
+     * Менюшка под постом,
+     * запрашивающим подтверждение указанного количества товара
+     */
+    public TelegramBotInlineMenu createQuantityOkButton(String productId){
+        return createButton(MENU_ORDER_QUANTITY_OK_TEXT, CALLBACK_ORDER_QUANTITY_OK_HEADER, productId);
+    }
+
+
+    /**
+     * Менюшка под постом
+     * запрашивающим подтверждение указанного количества товара,
+     * предполагающая, что это последний товар
+     */
+    public TelegramBotInlineMenu createFinishButton(String productId){
+        return createButton(MENU_ORDER_FINISH_TEXT, CALLBACK_ORDER_ADD_HEADER, productId);
+    }
+
+    /**
+     * Менюшка под постом, подтверждающим указанный адрес
+     */
+    public TelegramBotInlineMenu createAddressOkButton(String productId){
+        return createButton(MENU_ORDER_ADDRESS_TEXT, CALLBACK_ORDER_ADDRESS_HEADER, productId);
     }
 }
