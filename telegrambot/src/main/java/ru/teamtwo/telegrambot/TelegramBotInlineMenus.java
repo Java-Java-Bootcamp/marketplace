25374ebbeea5327d056a9864cf39946aa9d72a58
Скворцов Демьян Алексеевич <6337520+slow3586@users.noreply.github.com>
2022-04-11 19:35:52 +0700

    менюшки попытка 2

--- telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegramBotInlineMenus.java
+++ telegrambot/src/main/java/ru/teamtwo/telegrambot/TelegramBotInlineMenus.java
@@ -7,13 +7,11 @@ public class TelegramBotInlineMenus {
 
     public static final String MENU_ORDER_DETAILS_TEXT = "Заказать";
     public static final String MENU_ORDER_ADD_TEXT = "Добавить в корзину";
-    public static final String MENU_ORDER_QUANTITY_OK_TEXT = "Подтвердить";
-    public static final String MENU_ORDER_FINISH_TEXT = "Закончить выбор";
-    public static final String MENU_ORDER_ADDRESS_TEXT = "Подтвердить адрес";
+    public static final String MENU_ORDER_FINISH_TEXT = "Оформить заказ";
+    public static final String MENU_ORDER_ADDRESS_TEXT = "Подтвердить";
 
     public static final String CALLBACK_ORDER_DETAILS_HEADER = "CallbackOrderDetails";
     public static final String CALLBACK_ORDER_ADD_HEADER = "CallbackOrderAdd";
-    public static final String CALLBACK_ORDER_QUANTITY_OK_HEADER = "CallbackOrderQuantity";
     public static final String CALLBACK_ORDER_FINISH_HEADER = "CallbackOrderFinish";
     public static final String CALLBACK_ORDER_ADDRESS_HEADER = "CallbackOrderAddress";
 
@@ -40,15 +38,6 @@ public class TelegramBotInlineMenus {
     public TelegramBotInlineMenu createAddButton(String productId){
         return createButton(MENU_ORDER_ADD_TEXT, CALLBACK_ORDER_FINISH_HEADER, productId);
     }
-
-    /**
-     * Менюшка под постом,
-     * запрашивающим подтверждение указанного количества товара
-     */
-    public TelegramBotInlineMenu createQuantityOkButton(String productId){
-        return createButton(MENU_ORDER_QUANTITY_OK_TEXT, CALLBACK_ORDER_QUANTITY_OK_HEADER, productId);
-    }
-
 
     /**
      * Менюшка под постом
