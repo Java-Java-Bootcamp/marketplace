0afc526218b170ede5bca4b3635450635c59d6ac
Скворцов Демьян Алексеевич <6337520+slow3586@users.noreply.github.com>
2022-04-12 18:53:01 +0700

    UserState и handler

---
+++ telegrambot/src/main/java/ru/teamtwo/telegrambot/UserState.java
@@ -0,0 +1,51 @@
+package ru.teamtwo.telegrambot;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * Хранит в себе информацию о состоянии
+ * взаимодействия с пользователем: на какой стадии поиска
+ * он находится и т.д.
+ */
+@Data
+@NoArgsConstructor
+public class UserState {
+
+    private static final int DEFAULT_LIMIT = 5;
+    private static final int DEFAULT_OFFSET = 0;
+
+    public enum State{
+        WAITING_FOR_SEARCH_START,
+        WAITING_FOR_SEARCH_QUERY,
+        WAITING_FOR_SORTING_TYPE_FIELD,
+        WAITING_FOR_SORTING_TYPE_ASCDESC,
+        WAITING_FOR_ADD_OR_FINISH,
+        WAITING_FOR_QUANTITY,
+        WAITING_FOR_ADDRESS
+    }
+
+    private String chatId = "";
+    private State state = State.WAITING_FOR_SEARCH_START;
+    private String searchQuery = "";
+    private TelegramBotRESTHandler.OrderType orderType;
+    private TelegramBotRESTHandler.OrderTypeAscDesc orderTypeAscDesc;
+    private int offset = DEFAULT_OFFSET;
+    private int limit = DEFAULT_LIMIT;
+    private Map<String, Integer> cart;
+    private String currentProductId = "";
+
+    /**
+     * Сбрасывает все до стандартных значений
+     */
+    public void reset(){
+        this.setState(UserState.State.WAITING_FOR_SEARCH_START);
+        this.setSearchQuery("");
+        this.getCart().clear();
+        this.setOffset(DEFAULT_OFFSET);
+        this.setLimit(DEFAULT_LIMIT);
+        this.setCurrentProductId("");
+    }
+}
