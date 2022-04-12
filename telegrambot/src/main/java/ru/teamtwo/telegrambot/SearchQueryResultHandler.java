c7fbff83e52760c9bfed9c1bf0661e5169f9c61a
Ykr8891 <ykr8891@gmail.com>
2022-04-12 16:59:37 +0300

    move menus to ru.teamtwo.telegrambot.menus package

--- telegrambot/src/main/java/ru/teamtwo/telegrambot/SearchQueryResultHandler.java
+++ telegrambot/src/main/java/ru/teamtwo/telegrambot/SearchQueryResultHandler.java
@@ -1,11 +1,16 @@
 package ru.teamtwo.telegrambot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.teamtwo.telegrambot.dtos.ProductDTO;
+import ru.teamtwo.telegrambot.menus.TelegramBotSortMenus;
 
 import java.util.List;
+
+import static ru.teamtwo.telegrambot.menus.TelegramBotSortMenus.getSortByAscDescKeyboard;
+import static ru.teamtwo.telegrambot.menus.TelegramBotSortMenus.getSortByFieldKeyboard;
 
 @Component
 public class SearchQueryResultHandler {
@@ -24,6 +29,9 @@ public class SearchQueryResultHandler {
             }
             String messageText = sb.toString();
             message.setText(messageText);
+//          отдать пользователю меню сортировки
+            message.setReplyMarkup(getSortByFieldKeyboard());
+//          сохранить сообщение пользователя, чтобы потом отправить текст с новым пармтером сортировки
 
             }
 
