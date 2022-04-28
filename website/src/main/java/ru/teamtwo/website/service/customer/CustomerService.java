2874afef80ea9002e7d6cafc23c650badc8b6142
Скворцов Демьян Алексеевич <6337520+slow3586@users.noreply.github.com>
2022-04-28 18:33:47 +0700

    база тестов

--- website/src/main/java/ru/teamtwo/website/service/customer/CustomerService.java
+++ website/src/main/java/ru/teamtwo/website/service/customer/CustomerService.java
@@ -3,12 +3,15 @@ package ru.teamtwo.website.service.customer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import ru.teamtwo.core.dtos.customer.CustomerDto;
 import ru.teamtwo.core.models.customer.Customer;
 import ru.teamtwo.website.repository.customer.CustomerRepository;
 
 @Slf4j
 @Service
+@Transactional(propagation = Propagation.REQUIRES_NEW)
 public class CustomerService {
 
     @Autowired
